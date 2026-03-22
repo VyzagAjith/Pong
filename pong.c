8a1e17a3c8bcf00440415ef393088e7e19daab63
VyzagAjith <vyzagajith2k19@gmail.com>
2026-03-22 11:51:58 +0530

    Made the Game Screen and the paddles

--- pong.c
+++ pong.c
@@ -18,15 +18,16 @@ int main() {
     int x1 = (cols - strlen(text1)) / 2;
     int x2 = (cols - strlen(text2)) / 2;
     int visible = 1;
+    int counter = 0; 
 
     //MENU SCREEN
-    timeout(100);
+    timeout(100); // ~10 frames per second 
     if (y < rows && x1 < cols && x2 < cols){
-	clear();
-	attron(A_BOLD);
-	mvprintw(y, x1, "%s", text1);
-	attroff(A_BOLD);
 	while(1) {
+	    clear();
+	    attron(A_BOLD);
+	    mvprintw(y, x1, "%s", text1);
+	    attroff(A_BOLD);
 	    mvprintw(y + 1, x2, "Press ");
 
 	    if  (visible)
@@ -35,9 +36,13 @@ int main() {
 		mvprintw(y + 1, x2 + 6, "     ");
 
 	    mvprintw(y + 1, x2 + 11, " to play");
-
-	    visible = !visible; 
 	    
+	    //Blink Speed
+	    counter++;
+	    if (counter >= 5){
+	    visible = !visible;
+	    counter = 0;
+	    }
 	    refresh();
 	    int ch = getch();
 	    if(ch == '\n' || ch == KEY_ENTER)
@@ -56,7 +61,39 @@ int main() {
     refresh();
 
     //GAME SCREEN
-    mvprintw(0, 0, "GAME SCREEN");
+    int center_y = rows / 2;
+    int center_x = cols / 2;
+    mvprintw(0, 0, "Press ");
+    attron(A_BOLD);
+    mvprintw(0, 6, "Q");
+    attroff(A_BOLD);
+    mvprintw(0, 7, " to Quit");
+
+    for (int y_axis = 0; y_axis != rows + 1; y_axis++) 
+	mvprintw(y_axis, center_x, "|");  // Center Line
+    
+    //Ball 
+    attron(A_BOLD);
+    mvprintw(center_y, center_x, "O");
+    attroff(A_BOLD);
+
+    //Player Paddle
+    mvprintw(center_y, 0, "|"); //At start
+    int player_paddle_pos = center_y;
+    if (player_paddle_pos < rows - 1 && player_paddle_pos > 0){
+	mvprintw(player_paddle_pos - 1, 0, "|");
+	mvprintw(player_paddle_pos + 1, 0, "|");
+    }
+    
+    //Computer Paddle
+    mvprintw(center_y, cols - 1, "|");
+    int computer_paddle_pos = center_y;
+    if (computer_paddle_pos < rows - 1 && computer_paddle_pos > 0){
+	mvprintw(computer_paddle_pos - 1, cols - 1, "|");
+	mvprintw(computer_paddle_pos + 1, cols - 1, "|");
+    }
+
+
     refresh();
     timeout(-1);
     while (1){
